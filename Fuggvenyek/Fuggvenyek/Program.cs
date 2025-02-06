8b64422946296cec5b670857203541a1fb46e958
kovacsrud <kovacs.rud@gmail.com>
2025-02-06 10:35:33 +0100

    2d tombok, függvények

---
+++ Fuggvenyek/Fuggvenyek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fuggvenyek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Függvények");
+        }
+    }
+}
---
+++ KetdimenziosTombok/KetdimenziosTombok/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace KetdimenziosTombok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kétdimenziós tömbök");
+
+            //Deklaráció
+            int[,] szamok2d = new int[15,20];
+            //Véletlenszám generálása
+            Random rnd = new Random();
+
+            Console.WriteLine(szamok2d.GetLength(0));
+
+            //Feltöltés adatokkal
+            for (int i = 0; i < szamok2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < szamok2d.GetLength(1); j++)
+                {
+                    szamok2d[i, j] = rnd.Next(10, 100);
+                }
+            }
+
+
+            //Kiíratás
+            for (int i = 0; i < szamok2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < szamok2d.GetLength(1); j++)
+                {
+                    Console.Write(szamok2d[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}

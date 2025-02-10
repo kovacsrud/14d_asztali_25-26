2e5393c26966bd5d4f03529463e057c83c3b3d46
kovacsrud <kovacs.rud@gmail.com>
2025-02-10 14:18:41 +0100

    Listák

---
+++ Listak/ListaFunkciok/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ListaFunkciok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista funkciók");
+            List<int> szamok=new List<int>();
+            Random rand = new Random();
+
+            int darab=rand.Next(100,200+1);
+
+            for (int i = 0; i < darab; i++) {
+                szamok.Add(rand.Next(-100,200+1));
+            }
+
+            Console.WriteLine($"A lista hossza:{szamok.Count}");
+
+            //Készítsünk egy új listát, amelyik az eredeti listából az 50-nél nagyobbakat tartalmazza.
+            List<int> nagyobbMintOtven = szamok.FindAll(x=>x>50);
+
+            Listazas(nagyobbMintOtven);
+            
+            var kisebbMintNulla=szamok.FindAll(x=>x<0);
+
+            Console.WriteLine(kisebbMintNulla);
+
+            Listazas(kisebbMintNulla);
+
+            //A darabszám értéke megtalálható-e a listában?
+            if (szamok.Contains(darab))
+            {
+                Console.WriteLine($"A darabszám {darab} értéke benne van a listában!");
+            } else
+            {
+                Console.WriteLine($"A darabszám {darab} értéke nincs benne a listában!");
+            }
+
+            //Van-e 179-nél nagyobb szám a listában?
+            if (szamok.Any(x=>x>179))
+            {
+                Console.WriteLine("Van 179-nél nagyobb");
+
+            } else
+            {
+                Console.WriteLine("Nincs 179-nél nagyobb");
+            }
+
+            //Van-e 179 és 185 közé eső szám a listában?
+            if (szamok.Any(x => x > 179 && x<185))
+            {
+                Console.WriteLine("Van ilyen");
+
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen");
+            }
+
+
+
+        }
+
+        private static void Listazas(List<int> nevek)
+        {
+            for (int i = 0; i < nevek.Count; i++)
+            {
+                Console.Write(nevek[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Listak/Listak/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Listak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listák használata");
+
+            List<string> nevek = new List<string>();
+
+            //Új elem hozzáadása a listához:
+            nevek.Add("Ubul");
+            nevek.Add("Ella");
+            nevek.Add("Péter");
+            nevek.Add("Géza");
+
+            Console.WriteLine($"A lista hossza:{nevek.Count}");
+
+            //A lista elemei ugyanúgy indexelhetők, mint a tömb elemei
+            Console.WriteLine(nevek[0]); ;
+
+            //Lista bejárása for ciklussal
+            Nevlista(nevek);
+
+            //Lista bejárása foreach ciklussal
+            NevlistaForeach(nevek);
+
+            //A listáról törölhetünk elemet
+            nevek.Remove("Péter");
+
+            //Törlés a listáról index alapján
+            nevek.RemoveAt(2);
+
+            Nevlista(nevek);
+
+            Console.WriteLine(nevek.Count);
+
+            List<string> masikNevek = new List<string>() { "János", "Zalán", "Júlia" };
+
+            Nevlista(masikNevek);
+
+            //Lista hozzáadása egy másik listához
+            masikNevek.AddRange(nevek);
+
+            Nevlista(masikNevek);
+
+            masikNevek.Clear();
+
+            Console.WriteLine(masikNevek.Count);
+
+
+
+      
+            
+
+
+        }
+
+        private static void NevlistaForeach(List<string> nevek)
+        {
+            foreach (var i in nevek)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void Nevlista(List<string> nevek)
+        {
+            for (int i = 0; i < nevek.Count; i++)
+            {
+                Console.Write(nevek[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

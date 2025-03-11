6c46eef2622af845486ddba6a88c52aba04d9966
kovacsrud <kovacs.rud@gmail.com>
2025-03-11 09:19:38 +0100

    Időjárásadatok kezelése

---
+++ Idojaras/Idojaras/IdojarasLista.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idojaras
+{
+    public class IdojarasLista
+    {
+        public List<IdojarasAdat> IdojarasAdatok { get; set; } = new List<IdojarasAdat>();
+
+        public IdojarasLista(string fajl,char hatarolo,int start=1)
+        {
+            //Hibakezelés?
+            var sorok=File.ReadAllLines(fajl,Encoding.Default);
+
+            for (int i = start; i < sorok.Length; i++)
+            {
+                IdojarasAdatok.Add(new IdojarasAdat(sorok[i], hatarolo));
+            }
+        }
+    }
+}
---
+++ Idojaras/Idojaras/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Idojaras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IdojarasAdat> IdojarasAdatok= new List<IdojarasAdat>();
+            try
+            {
+                IdojarasLista idojarasLista = new IdojarasLista("idojaras.csv",';');
+                //Ha az első sor is fel akarom dolgozni a fájlból, akkor így példányosítok
+                //IdojarasLista idojarasLista = new IdojarasLista("idojaras.csv",';',0);
+                IdojarasAdatok = idojarasLista.IdojarasAdatok;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"Sorok száma:{IdojarasAdatok.Count}");
+
+            //Melyik volt a legmelegebb nap?
+            
+            var maxHo = IdojarasAdatok.Max(y => y.Homerseklet);
+
+            var legmelegebbNap = IdojarasAdatok.Find(x=>x.Homerseklet==maxHo);
+            if (legmelegebbNap!=null)
+            {
+                Console.WriteLine($"A legmelegebb nap:{legmelegebbNap.Ev}.{legmelegebbNap.Honap}.{legmelegebbNap.Nap} {legmelegebbNap.Ora} - {legmelegebbNap.Homerseklet}");
+            }
+
+            //Határozzuk meg minden egyes nap átlaghőmérsékletét
+
+            //Összesítés összetett kulccsal
+            var atlaghoStat = IdojarasAdatok.ToLookup(x=>new {x.Ev,x.Honap,x.Nap});
+
+            foreach (var i in atlaghoStat)
+            {
+                Console.WriteLine($"{i.Key.Ev}.{i.Key.Honap}.{i.Key.Nap} - {i.Average(x=>x.Homerseklet)} - {i.Max(x=>x.Homerseklet)} - {i.Min(x=>x.Homerseklet)}");
+            }
+
+        }
+    }
+}

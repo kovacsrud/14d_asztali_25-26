65cbcddd1a65b558c4dd05dad6cf92135ea0342b
kovacsrud <kovacs.rud@gmail.com>
2025-03-20 13:34:11 +0100

    Wpf textchange

---
+++ WpfTextChange/WpfTextChange/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTextChange;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void textboxSzoveg_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        textblockMasolat.Text = textboxSzoveg.Text;
+        textblockHossz.Text= textboxSzoveg.Text.Length.ToString()+" db karakter";
+        textboxCsakOlvashato.Text = textboxSzoveg.Text;
+    }
+
+    private void buttonKapcsol_Click(object sender, RoutedEventArgs e)
+    {
+        textboxCsakOlvashato.IsReadOnly = !textboxCsakOlvashato.IsReadOnly;
+        if (textboxCsakOlvashato.IsReadOnly)
+        {
+            buttonKapcsol.Content = "Legyen írható";
+        } else
+        {
+            buttonKapcsol.Content = "Legyen olvasható";
+        }
+    }
+}

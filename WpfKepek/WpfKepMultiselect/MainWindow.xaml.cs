ae818f2ff55a80c6e767e9b48e7d036e1504ccee
kovacsrud <kovacs.rud@gmail.com>
2025-03-31 14:41:07 +0200

    Képek kezelése WPF-ben

---
+++ WpfKepek/WpfKepMultiselect/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKepMultiselect;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void buttonOpen_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = ".jpg|*.jpg|.png|*.png|összes fájl|*.*";
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog()==true)
+            {
+                for (int i = 0; i < dialog.FileNames.Length; i++)
+                {
+                    Image kep = new Image();
+                    kep.Source = new BitmapImage(new Uri(dialog.FileNames[i]));
+                    kep.Stretch = Stretch.UniformToFill;
+                    wrapKepek.Children.Add(kep);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);            
+        }
+    }
+
+    private void buttonClear_Click(object sender, RoutedEventArgs e)
+    {
+        wrapKepek.Children.Clear();
+    }
+}
---
+++ WpfKepek/WpfKepek/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKepek;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        //imageMasodik.Source = "D:\\rud-2\\kodtarak_2024\\13d_asztali_24-25\\WpfKepek\\WpfKepek\\bin\\Debug\\net8.0-windows\\red_joker.png";
+
+        imageMasodik.Source = new BitmapImage(new Uri(@"D:\rud-2\kodtarak_2024\13d_asztali_24-25\WpfKepek\WpfKepek\bin\Debug\net8.0-windows\red_joker.png"));
+        imageMasodik.Stretch = Stretch.Uniform;
+    }
+
+    private void buttonOpen_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            OpenFileDialog dialog= new OpenFileDialog();
+            dialog.Filter = ".jpg|*.jpg|.png|*.png|összes fájl|*.*";
+
+            if (dialog.ShowDialog() == true) {
+                imageHarmadik.Source = new BitmapImage(new Uri(dialog.FileName));
+                this.Title= dialog.FileName.Split('\\').Last();
+
+                //A fájlnév kiszedése a teljes elérési útból
+                //var eleresiutElemek=dialog.FileName.Split('\\');
+                //var fajlnev = eleresiutElemek.Last();
+                //this.Title = fajlnev;
+
+            
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);            
+        }
+    }
+}

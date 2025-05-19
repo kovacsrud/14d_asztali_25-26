3f1a7f69569f50afe5408e16d8e7d9eb4542d3b3
kovacsrud <kovacs.rud@gmail.com>
2025-05-19 13:31:49 +0200

    Időzítők használata

---
+++ Idozitok/KonzolIdozito/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Timers;
+
+namespace KonzolIdozito
+{
+    internal class Program
+    {
+        static int szamlalo = 0;
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(2000);
+            timer.Elapsed += Callback;
+            timer.Start();
+            Console.ReadKey();
+            
+        }
+
+        private static void Callback(object sender, ElapsedEventArgs e) {
+
+            Console.WriteLine($"{szamlalo}-{DateTime.Now.Second}");
+            szamlalo++;
+
+        }
+    }
+}
---
+++ Idozitok/WpfIdozito/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfIdozito;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    DispatcherTimer timer;
+    public MainWindow()
+    {
+        InitializeComponent();
+        timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, Valtas,Dispatcher.CurrentDispatcher);
+        timer.Stop();
+    }
+
+    private void Valtas(object sender, EventArgs e)
+    {
+        if (textblockA.Visibility==Visibility.Visible)
+        {
+            textblockA.Visibility = Visibility.Hidden;
+            textblockB.Visibility = Visibility.Visible;
+        } else
+        {
+            textblockA.Visibility = Visibility.Visible;
+            textblockB.Visibility = Visibility.Hidden;
+        }
+    }
+
+    private void buttonKapcsol_Click(object sender, RoutedEventArgs e)
+    {
+        timer.IsEnabled = !timer.IsEnabled;
+        if (timer.IsEnabled)
+        {
+            buttonKapcsol.Content = "Stop";
+        } else
+        {
+            buttonKapcsol.Content = "Start";
+        }
+    }
+}

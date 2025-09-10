24173c8652ed0e574cb77f483179621a68954f83
kovacsrud <kovacs.rud@gmail.com>
2025-09-10 10:36:00 +0200

    Szűrések

--- WpfMobilok/WpfMobilok/Views/GyartoSzuresView.xaml.cs
+++ WpfMobilok/WpfMobilok/Views/GyartoSzuresView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +46,50 @@ namespace WpfMobilok.Views
         private void buttonOsszesAdat_Click(object sender, RoutedEventArgs e)
         {
             DatagridGyartoSzures.ItemsSource=this.DataContext as List<Mobil>;
+        }
+
+        private void buttonModellKeres_Click(object sender, RoutedEventArgs e)
+        {
+            var keresettModell = textboxModell.Text;
+
+            var eredmeny = (this.DataContext as List<Mobil>).FindAll(x => x.Modell.ToLower().Contains(keresettModell.ToLower()));
+
+            if (eredmeny.Count > 0)
+            {
+                DatagridGyartoSzures.ItemsSource = eredmeny;
+            }
+            else
+            {
+                DatagridGyartoSzures.ItemsSource = null;
+                MessageBox.Show("Nincs a feltételnek megfelelő adat!");
+                DatagridGyartoSzures.ItemsSource = this.DataContext as List<Mobil>;
+            }
+        }
+
+        private void buttonMentes_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = ".csv fájlok|*.csv|.txt fájlok|*.txt";
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    FileStream fajl = new FileStream(dialog.FileName, FileMode.Create);
+                    using (StreamWriter writer = new StreamWriter(fajl, Encoding.UTF8))
+                    {
+                        writer.WriteLine("Id;Nev;Gyarto;Modell;Oprendszer;Datum");
+                        foreach (var i in DatagridGyartoSzures.ItemsSource as List<Mobil>)
+                        {
+                            writer.WriteLine($"{i.Id};{i.Nev};{i.Gyarto};{i.Modell};{i.OS};{i.GyartasiEv}");
+                        }
+                    }
+                    MessageBox.Show("Kiírás kész!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }

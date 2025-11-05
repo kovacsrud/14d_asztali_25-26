7743535a055b71d30d75ebbf25c7369f87d02303
kovacsrud <kovacs.rud@gmail.com>
2025-11-05 09:12:35 +0100

    LoadMauiAsset függvény megírása

--- MauiCollection14d2025/MauiCollection14d2025/MainPage.xaml.cs
+++ MauiCollection14d2025/MauiCollection14d2025/MainPage.xaml.cs
@@ -1,12 +1,26 @@
-﻿namespace MauiCollection14d2025
+﻿using System.Collections.ObjectModel;
+
+namespace MauiCollection14d2025
 {
     public partial class MainPage : ContentPage
     {
        
+        ObservableCollection<Hegy> Hegyek {  get; set; }=new ObservableCollection<Hegy>();
 
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        async Task LoadMauiAsset()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("hegyekMo.txt");
+            using var reader = new StreamReader(stream);
+            reader.ReadLine();
+            while (!reader.EndOfStream) {
+                Hegyek.Add(new Hegy(reader.ReadLine()));
+            }
         }
 
       

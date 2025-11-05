0409f1a58d310148e563205cf2f2b7f7f056e102
kovacsrud <kovacs.rud@gmail.com>
2025-11-05 10:05:13 +0100

    ListaPage kódjának megadása

--- MauiCollection14d2025/MauiCollection14d2025/ListaPage.xaml.cs
+++ MauiCollection14d2025/MauiCollection14d2025/ListaPage.xaml.cs
@@ -1,9 +1,31 @@
+using System.Collections.ObjectModel;
+
 namespace MauiCollection14d2025;
 
 public partial class ListaPage : ContentPage
 {
-	public ListaPage()
+    ObservableCollection<Hegy> Hegyek { get; set; } = new ObservableCollection<Hegy>();
+    public ListaPage()
 	{
 		InitializeComponent();
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadMauiAsset();
+        BindingContext = Hegyek;
+    }
+
+
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("hegyekMo.txt");
+        using var reader = new StreamReader(stream);
+        reader.ReadLine();
+        while (!reader.EndOfStream)
+        {
+            Hegyek.Add(new Hegy(reader.ReadLine()));
+        }
+    }
 }

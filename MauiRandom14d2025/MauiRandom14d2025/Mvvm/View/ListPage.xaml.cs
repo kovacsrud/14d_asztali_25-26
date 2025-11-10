73dac6cefc279144bb848a0fe7a9ad727533bec4
kovacsrud <kovacs.rud@gmail.com>
2025-11-10 10:57:08 +0100

    Adatok lekérése, megjelenítése

--- MauiRandom14d2025/MauiRandom14d2025/Mvvm/View/ListPage.xaml.cs
+++ MauiRandom14d2025/MauiRandom14d2025/Mvvm/View/ListPage.xaml.cs
@@ -1,9 +1,50 @@
+using MauiRandom14d2025.Mvvm.Model;
+using System.Text.Json;
+
 namespace MauiRandom14d2025.Mvvm.View;
 
 public partial class ListPage : ContentPage
 {
+	HttpClient client;
+	JsonSerializerOptions serializerOptions;
+	string baseurl = "https://randomuser.me/api";
+
 	public ListPage()
 	{
 		InitializeComponent();
+		client = new HttpClient();
+		serializerOptions = new JsonSerializerOptions { WriteIndented=true };
 	}
+
+    private async void buttonGet_Clicked(object sender, EventArgs e)
+    {
+		var url = $"{baseurl}/?results=50";
+
+		if (Connectivity.Current.NetworkAccess==NetworkAccess.Internet) {
+			//lekérjük az adatokat
+			try
+			{
+				var response = await client.GetAsync(url);
+				if (response.IsSuccessStatusCode) {
+					using (var responseStream=await response.Content.ReadAsStreamAsync())
+					{
+						var data = await JsonSerializer.DeserializeAsync<RandomUsers>(responseStream,serializerOptions);
+						collectionUsers.ItemsSource = data.results;
+					}
+
+				} else
+				{
+					collectionUsers.EmptyView = "No data";
+				}
+			}
+			catch (Exception ex)
+			{
+				DisplayAlert("Hiba", ex.Message, "Ok");
+			}
+		} else
+		{
+			collectionUsers.EmptyView = "Nincs internet hozzáférés!";
+		}
+
+    }
 }

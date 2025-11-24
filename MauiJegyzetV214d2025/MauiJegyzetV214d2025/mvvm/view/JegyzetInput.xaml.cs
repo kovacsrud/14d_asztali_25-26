6f2ad4c27324ea9e56289e9ca046e477cce51146
kovacsrud <kovacs.rud@gmail.com>
2025-11-24 13:11:14 +0100

    Új jegyzet felvitele

---
+++ MauiJegyzetV214d2025/MauiJegyzetV214d2025/mvvm/view/JegyzetInput.xaml.cs
@@ -0,0 +1,43 @@
+using MauiJegyzetV214d2025.mvvm.viewmodel;
+
+namespace MauiJegyzetV214d2025.mvvm.view;
+
+public partial class JegyzetInput : ContentPage
+{
+	bool modosit=false;
+	public JegyzetInput()
+	{
+		InitializeComponent();
+	}
+
+    public JegyzetInput(bool modosit)
+    {
+        InitializeComponent();
+        this.modosit = modosit;
+    }
+
+    private async void buttonInput_Clicked(object sender, EventArgs e)
+    {
+        var vm = BindingContext as JegyzetViewModel;
+
+        if (modosit) {
+            //módósítás
+            labelCim.Text = "Jegyzet módosítása";
+
+            var result =await DisplayAlert("Módosítás", "Biztosan módosítja?", "Igen","Nem");
+
+            if (result)
+            {
+                App.JegyzetRepo.UpdateItem(vm.AktualisJegyzet);
+                vm.GetJegyzetek();
+            }
+
+        } else
+        {
+            //új jegyzet
+            App.JegyzetRepo.NewItem(new model.Jegyzet { Cim=entryCim.Text,Szoveg=entrySzoveg.Text});
+            vm.GetJegyzetek();
+
+        }
+    }
+}
--- MauiJegyzetV214d2025/MauiJegyzetV214d2025/mvvm/view/JegyzetView.xaml.cs
+++ MauiJegyzetV214d2025/MauiJegyzetV214d2025/mvvm/view/JegyzetView.xaml.cs
@@ -1,3 +1,5 @@
+using MauiJegyzetV214d2025.mvvm.viewmodel;
+
 namespace MauiJegyzetV214d2025.mvvm.view;
 
 public partial class JegyzetView : ContentPage
@@ -5,5 +7,17 @@ public partial class JegyzetView : ContentPage
 	public JegyzetView()
 	{
 		InitializeComponent();
+        BindingContext = new JegyzetViewModel();
 	}
+
+    private void buttonUj_Clicked(object sender, EventArgs e)
+    {
+        var vm=BindingContext as JegyzetViewModel;
+        Navigation.PushAsync(new JegyzetInput {BindingContext=vm });
+    }
+
+    private void buttonModosit_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }

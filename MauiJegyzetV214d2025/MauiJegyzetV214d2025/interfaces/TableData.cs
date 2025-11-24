e31675d23b6ce3645431f14d15691c16bae75f81
kovacsrud <kovacs.rud@gmail.com>
2025-11-24 10:52:16 +0100

    Modell osztályok létrehozása

---
+++ MauiJegyzetV214d2025/MauiJegyzetV214d2025/interfaces/TableData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiJegyzetV214d2025.interfaces
+{
+    public class TableData
+    {
+        [PrimaryKey, AutoIncrement]
+        public int  Id { get; set; }
+    }
+}
---
+++ MauiJegyzetV214d2025/MauiJegyzetV214d2025/mvvm/model/Jegyzet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiJegyzetV214d2025.interfaces;
+using SQLite;
+
+namespace MauiJegyzetV214d2025.mvvm.model
+{
+    [System.ComponentModel.DataAnnotations.Schema.Table("jegyzetek")]
+    public class Jegyzet:TableData
+    {
+        [NotNull]
+        public string Cim { get; set; }
+        [NotNull]
+        public string Szoveg { get; set; }
+        [NotNull]
+        public DateTime Datum { get; set; }
+    }
+}

fad67b5d641cb7c7507ca90f27a2507633e450a5
kovacsrud <kovacs.rud@gmail.com>
2025-12-08 10:08:08 +0100

    Fájlok kezelése binárisan

---
+++ BinarisFajlkezeles/BinarisFajlkezeles/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace BinarisFajlkezeles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var szovegFajl = File.ReadAllBytes("szinek.txt");
+
+            foreach (var bajt in szovegFajl)
+            {
+                //Console.Write(bajt+" ");
+                //Console.Write(Convert.ToString(bajt,2).PadLeft(8,'0')+" ");
+            }
+
+            //Console.WriteLine(Encoding.UTF8.GetString(szovegFajl));
+
+            var zipFajl = File.ReadAllBytes("MonthyHallGame.zip");
+
+            foreach(var bajt in zipFajl)
+            {
+                //Console.Write(Convert.ToString(bajt, 2).PadLeft(8, '0') + " ");
+            }
+
+            //Console.WriteLine(Encoding.UTF8.GetString(zipFajl));
+
+            using (MemoryStream ms=new MemoryStream(zipFajl))
+            {
+                using (BinaryReader br=new BinaryReader(ms))
+                {
+                    var signature = br.ReadBytes(4);
+                    Console.WriteLine($"Signature:{BitConverter.ToString(signature)}");
+                    var version = br.ReadBytes(2);
+                    Console.WriteLine($"Version:{BitConverter.ToUInt16(version)}");
+                    var flags = br.ReadBytes(2);
+                    Console.WriteLine($"Flags:{BitConverter.ToUInt16(version)}");
+                    var compression= br.ReadBytes(2);
+                    Console.WriteLine($"Compression:{BitConverter.ToUInt16(compression)}");
+                    var modtime= br.ReadBytes(2);
+                    Console.WriteLine($"Mod time:{BitConverter.ToUInt16(modtime)}");
+                    var moddate= br.ReadBytes(2);
+                    Console.WriteLine($"Mod date:{BitConverter.ToUInt16(moddate)}");
+
+
+                }
+            }
+
+        }
+    }
+}

fc00ce61ecae37f2b3867dcfb84398b7670067bd
kovacsrud <kovacs.rud@gmail.com>
2025-12-11 14:12:31 +0100

    Titkosítás AES algoritmussal

---
+++ FajlTitkosito/FajlTitkosito/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FajlTitkosito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Titkosítás");
+
+
+            //Szükséges adatok
+            //A titkosítandó szöveg
+            //Eredeti fájlnév
+            //Inicializációs vektor
+            //tartalom hash
+            //titkosított tartalom
+
+            //Fájl formátuma
+            //IV - 16 byte//fájlnév hossza 4byte//fájlnév - változó//tartalom hash - 32byte//tartalom hossza -4byte //titkosított tartalom -változó
+
+            string fajlnev = "titkos.txt";
+            string kulcs = "Titok_12";
+            string titkositando = "Szigorúan titkos tartalom";
+
+            //Titkosító algoritmus
+            Aes aes=Aes.Create();
+
+            //Hash
+            SHA256 sha256=SHA256.Create();
+
+            byte[] binKulcs=sha256.ComputeHash(Encoding.UTF8.GetBytes(kulcs));
+            byte[] tartalomHash = sha256.ComputeHash(Encoding.UTF8.GetBytes(titkositando));
+            byte[] fajlnevBin=Encoding.UTF8.GetBytes(fajlnev);
+            byte[] titkositandoBin = Encoding.UTF8.GetBytes(titkositando);
+            int fajlnevHossz=fajlnevBin.Length;
+            aes.Key = binKulcs;
+            aes.GenerateIV();
+
+            ICryptoTransform titkosito = aes.CreateEncryptor(binKulcs, aes.IV);
+            byte[] titkositott = titkosito.TransformFinalBlock(titkositandoBin,0,titkositandoBin.Length);
+
+            string kodoltSzoveg = Encoding.UTF8.GetString(titkositott);
+            int tartalomHossz = titkositott.Length;
+
+            Console.WriteLine(kodoltSzoveg);
+
+            var kodoltAdatok = new byte[aes.IV.Length+fajlnevHossz+fajlnevBin.Length+tartalomHash.Length+tartalomHossz+titkositott.Length];
+
+
+        }
+    }
+}

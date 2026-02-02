36dfbeee95931294dae9d1792c5875b8a78200af
kovacsrud <kovacs.rud@gmail.com>
2026-02-02 10:55:49 +0100

    Térkép létrehozása

---
+++ WpfMagyarorszagMysql/WpfMagyarorszagMysql/mvvm/views/MapView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfMagyarorszagMysql.mvvm.viewmodel;
+
+namespace WpfMagyarorszagMysql.mvvm.views
+{
+    /// <summary>
+    /// Interaction logic for MapView.xaml
+    /// </summary>
+    public partial class MapView : Window
+    {
+        public MapView()
+        {
+            InitializeComponent();
+            var vm = DataContext as TelepulesViewModel;
+            map.Center = new Microsoft.Maps.MapControl.WPF.Location(vm.SelectedTelepules.Lat,vm.SelectedTelepules.Long);
+            map.ZoomLevel = 12;
+        }
+    }
+}

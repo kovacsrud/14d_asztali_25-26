f10054369b7e6d6f4a57296020e39ee85fd9c4b6
kovacsrud <kovacs.rud@gmail.com>
2026-02-11 09:09:06 +0100

    Model First példa

---
+++ WpfModelFirst/WpfModelFirst/mvvm/model/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfModelFirst.mvvm.model
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=d:/database/user.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        }
+    }
+}

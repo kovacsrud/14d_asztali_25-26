88326315becdeda13b6c226874d0188bc86f2567
kovacsrud <kovacs.rud@gmail.com>
2026-02-25 08:45:27 +0100

    ALap médialejátszó

---
+++ WpfMediaPlayer/WpfMediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaPlayer.LoadedBehavior = MediaState.Manual;
+            mediaPlayer.Source = new Uri(@"d:/database/mp4/spooky.mp4");
+            
+        }
+
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void buttonPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

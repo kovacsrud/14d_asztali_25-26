23913c36f8144e86320a07915c71054065b1a6c1
kovacsrud <kovacs.rud@gmail.com>
2026-03-02 09:54:32 +0100

    Update DolgozoViewModel.cs

--- WpfMunkaugyEF/WpfMunkaugyEF/mvvm/viewmodel/DolgozoViewModel.cs
+++ WpfMunkaugyEF/WpfMunkaugyEF/mvvm/viewmodel/DolgozoViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfMunkaugyEF.mvvm.model;
 
 namespace WpfMunkaugyEF.mvvm.viewmodel
@@ -16,5 +18,36 @@ namespace WpfMunkaugyEF.mvvm.viewmodel
         public ObservableCollection<Nyilvantarta> Nyilvantartas { get; set; }
 
         public Dolgozo SelectedDolgozo { get; set; }
+
+        public DolgozoViewModel()
+        {
+            context=new MunkaNyilvantartasContext();
+            context.Nyilvantartas.Load();
+            context.Reszlegs.Load();
+            context.Dolgozos.Load();
+            Dolgozok = context.Dolgozos.Local.ToObservableCollection();
+            Reszlegek=context.Reszlegs.Local.ToObservableCollection();
+            Nyilvantartas=context.Nyilvantartas.Local.ToObservableCollection();
+
+        }
+
+        public void DbMentes()
+        {
+            try
+            {
+                var result = context.SaveChanges();
+                if (result>0)
+                {
+                    MessageBox.Show("Adatok mentve!");
+                } else
+                {
+                    MessageBox.Show("Nem változtak az adatok!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
     }
 }
